8230a983b6fb6287f8de36ccea1857103f448766
Oleksii Zozulenko <keemdek@gmail.com>
2026-07-30 22:47:40 +0300

    Enhance diagram and main code structure; add libraries list for components

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define RELAY_IRRIGATION_PIN 25
+#define RELAY_VENTILATION_PIN 26
+#define RELAY_LIGHT_PIN 27
+
+#define OLED_SDA_PIN 21
+#define OLED_SDC_PIN 22
+
+#define BUSSER_PIN  18
+//#define LED_PIN
+
+#define BTN_MODE_PIN 13
+#define BTN_IRRIGATION_PIN 12
+#define BTN_VENTILATION_PIN 14
+#define BTN_LIGHT_PIN 16
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(10); // this speeds up the simulation
 }
